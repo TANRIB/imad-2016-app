80e50c50067207ef4a2a572d991c770974bed5ef
TANRIB <kibafathima@gmail.com>
2016-10-28 17:12:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,166 +1,131 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
-
-var config = {
-    user : 'Riba Fathima',
-    database: 'Riba Fathima',
-    host: 'db.imad.hasura-app.io';
-    port: '5432';
-    password: process.env.DB_PASSWORD
-};
 
 var app = express();
 app.use(morgan('combined'));
 
 
 var articles = {
-    article_one: {
-    title: 'Riba Fathima |ArticleOne',
-    heading: 'Article One',
-    date: 'Sep 5 , 2016',
-    content : `
-    <p>
-        This is the content of my first article .This is the content of my first article .This is the content of my first article .This is the content of my first article .This is the content of my first article .
-            </p>
-             <p>
-                This is the content of my first article .This is the content of my first article .This is the content of my first article .This is the content of my first article .This is the content of my first article .
-            </p> `
-},
- article_two :{
-     title : '|ArticleTwo',
-    heading : 'Article Two',
-    date : 'Sep 10 , 2016',
-    content : `
-    <p>
-        This is second article !!!!!!
-            </p> `},
- article_three : {title : 'Riba Fathima|ArticleThree',
-    heading : 'Article Three',
-    date : 'Sep 15 , 2016',
-    content : `
-  
-    <p>
-              Johnny Johnny yes papa, Eating sugar no papa !!!
-            </p>
-            <p> Your feedback is precious </p>
-             `
- }
-  };
+    'article-one' : {
+      title:'Article One | Riba Fathima',
+      heading:'Article One',
+      date:'Sep 5, 2016',
+      content:`
+          <p>
+              This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+          </p>
+          <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+          </p>
+          <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+          </p>`
+   },
+    'article-two' : {
+          title:'Article Two | Riba Fathima',
+          heading:'Article Two',
+          date:'Sep 10, 2016',
+         content:`
+            <p>
+                This is the content for my second article.
+            </p>`    
+    },   
+    'article-three' : {
+       title:'Article Three | Riba Fathima',
+       heading:'Article Three',
+       date:'Sep 15, 2016',
+       content:`
+          <p>
+              This is the content for my third article.
+          </p>`
+    }  
+};  
 
-function createTemplate(data){
+function createTemplate (data) {
     var title = data.title;
+    var date  = data.date;
     var heading = data.heading;
-    var date = data.date;
     var content = data.content;
-
-        var htmlTemplate =  `<html>
-            <head>
-                <title>${title}</title>
-                <meta name = "viewport" content = "width = device-width initial-scale=1" />
-                  <link rel="SHORTCUT ICON" type="image/ico" href="" /> 
-                <link href="/ui/style.css" rel="stylesheet" />
-            </head>
-            <body>
-                <div class=container-articles>
-                    <a href="/">Home</a>
-                </div>
-                <hr />
-                <h1>${heading}</h1>
-                <div>
-                    ${date}
-                </div>
-                <div>
-                   ${content}
-                </div>
-                <br><hr>
-                  <div class = "commentbox">
-                <ul id="commentlist">
-                    
-                </ul>
-               <p align = "center"> <textarea id = "comment" rows = "8" tabindex = "4" placeholder = "Add a public comment" ></textarea> </p>
-               <br>
-               <p align = "center"><button id = "comment-button" >Submit</button></p>
-            </div> 
-               <script type="text/javascript" src="/ui/main.js">
-        </script>
-            </body>
-            
-            
-        </html> ` ;
-        return htmlTemplate;
+    
+var htmlTemplate = `
+  <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content ="width-device-width, initial-scale-1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+     </head>
+    <body>
+        <div class= "container">
+      <div>
+            <a href= "/">Home</a>
+        </div>
+         <hr/>
+         <h3>
+            ${heading}
+         </h3>
+         <div>
+             ${date} 
+         </div>
+         <div>
+            ${content}
+         </div>
+       </div>
+     </body>
+   </html>
+    `;
+    return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-// Testing connection to the database
-var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-   //make a select request and return result set
-   pool.query('SELECT * FROM test',function(err,result){
-       if(err){
-           res.status(500).send(err.toString());
-       }
-       else{
-           res.send(JSON.stringify(result.rows));
-       }
-       });
-       
-
+ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
-app.get('/counter',function(req,res) {
+var counter = 0;
+app.get('/counter', function (req,res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
 
-var names = [];
-app.get('/submit-name',function(req,res){
-   var name = req.query.name;//query does  is -> url ://submit-name?name=xxxxx;
-   
-   names.push(name);
-   
-   res.send(JSON.stringify(names));
-});
-//  HANDLING COMMENTS REQUEST RESPONSE
-var comments = [];
-app.get('/submit-comment',function(req,res){
-   var comment = req.query.comment;//query does  is -> url ://submit-comment?comment=xxxxx;
-    if (comments == undefined)
-  comments = [];  
-  comments.push(comment);
-   res.send(JSON.stringify(comments));
-});
-app.get('/fetchcomments', function(req, res) {
-  var comment = req.query.comment;
-  if (comments != undefined)
-    res.send(JSON.stringify(comments));
-  else {
-    res.send("null");
-  }
-});
-app.get('/ui/main.js', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+ var names = [];
+app.get('/submit-name', function(req , res) { // URL: /submit-name?name-xxxxx
+    // Get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+    // JSON: Javascript Object Notation
+    res.send(JSON.stringify(names));
 });
 
 app.get('/:articleName', function (req, res) {
-    var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+    // articleName == article-one
+    // articles[articleName] == {} content object for article one
+    var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
-
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});  
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/favicon.ico', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+
+var names = [];
+app.get('/submit-name', function(req , res) { // URL: /submit-name?name-xxxxx
+    // Get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+    // JSON: Javascript Object Notation
+    res.send(JSON.stringify(names));
 });
 
 
@@ -168,5 +133,3 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-Contact GitHub API Training Shop Blog About
-© 2016 GitHub, Inc. Term
