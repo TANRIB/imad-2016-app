b6cd6501fe72469a69d00f8de43f69fc4b97218b
TANRIB <kibafathima@gmail.com>
2016-10-28 15:58:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,21 +122,21 @@ app.get('/submit-name', function(req , res) { // URL: /submit-name?name-xxxxx
 });
 
 app.get('/articles/:articleName', function (req, res) {
-    // articleName == article-one
-    // articles[articleName] == {} content object for article one
+  // articleName == article-one
+  // articles[articleName] == {} content object for article one
     
-    // SELECT * FROM article WHERE title = 'article-one'
-   pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
-       if (err) {
-           res.status(500).send(err.toString());
-       } else {
-           if (result.rows.length === 0) {
-               res.status(404).send('Article not found');
-           } else {
-               var articleData = result.rows[0];
-               res.send(createTemplate(articleData));
-           }
-       }
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  pool.query("SELECT * FROM article WHERE title = $1", + [req.params.articleName], function (err, result) {
+    if (err) {
+         res.status(500).send(err.toString());
+    } else {
+         if (result.rows.length === 0) {
+             res.status(404).send('Article not found');
+        } else {
+             var articleData = result.rows[0];
+             res.send(createTemplate(articleData));
+        }
+    }
    });
 });    
 
