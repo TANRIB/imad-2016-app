f5d721941e2f16db32cb107a32b2f0760647fd8c
TANRIB <kibafathima@gmail.com>
2016-10-29 17:38:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,101 +5,111 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
 var articles = {
-    'article-one' : {
-      title:'Article One | Riba Fathima',
-      heading:'Article One',
-      date:'Sep 5, 2016',
-      content:`
-          <p>
-              This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-          </p>
-          <p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-          </p>
-          <p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+	'default': {
+		title: 'Home Page',
+		heading: 'Home Page',
+		date: 'Oct 2, 2016',
+		content:
+		  `<p>
+			  Hi..My name is A. Riba Fathima
+		  </p>`
+	}, 
+    'article-one': {
+      title: 'Article One',
+      heading: 'Article One',
+      date: 'Sep 5, 2016',
+      content: 
+			`<p>
+			     This is my first article
+			</p>`
+
+    },
+    'article-two': {
+      title: 'Article Two',
+      heading: 'Article Two',
+      date: 'Sep 10, 2016',
+      content: 
+          `<p>
+				This is my second article
           </p>`
-   },
-    'article-two' : {
-          title:'Article Two | Riba Fathima',
-          heading:'Article Two',
-          date:'Sep 10, 2016',
-         content:`
-            <p>
-                This is the content for my second article.
-            </p>`    
-    },   
-    'article-three' : {
-       title:'Article Three | Riba Fathima',
-       heading:'Article Three',
-       date:'Sep 15, 2016',
-       content:`
-          <p>
-              This is the content for my third article.
+    },
+    'article-three': {
+      title: 'Article Three',
+      heading: 'Article Three',
+      date: 'Sep 15, 2016',
+      content: 
+          `<p>
+				This is my third article
           </p>`
-    }  
-};  
+    }
+};
 
 function createTemplate (data) {
     var title = data.title;
-    var date  = data.date;
+    var date = data.date;
     var heading = data.heading;
     var content = data.content;
     
-var htmlTemplate = `
-  <html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content ="width-device-width, initial-scale-1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-     </head>
-    <body>
-       <div class = "footer">
-       This button <button id="counter">Click me!</button> has been clicked <span id ="count">0</span> times
-        <hr/>
-        <input type="text" id="name" placeholder="name"></input>
-        <input type="submit" value="Submit" id="Submit_btn"></input>
-        <ul id="namelist">
-        </ul>
-        </div>
-        </div>
-        <script type="text/javascript" src="/ui/main.js">
-        </script>
-        </body>
-        </html>
-`;
-return htmlTemplate;
+    var htmlTemplate = 
+	`
+      <h2>${heading}</h2>
+	  <p>${content}</p>	
+
+      <br>
+	`;
+
+    return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var counter = 0;
-app.get('/counter', function (req,res) {
-    counter = counter + 1;
-    res.send(counter.toString());
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
 });
 
- var names = [];
-app.get('/submit-name', function(req , res) { // URL: /submit-name?name-xxxxx
-    // Get the name from the request
-    var name = req.query.name;
-    
-    names.push(name);
-    // JSON: Javascript Object Notation
-    res.send(JSON.stringify(names));
+app.get('/currentctr', function (req, res) {
+   res.send(counter.toString());
+});
+
+var comments = [];
+
+
+app.get('/submit-comment', function(req, res) { 
+  // Get the name from the request
+  var comment = req.query.comment;
+  var context = req.query.context;
+	
+  var d = new Date();
+  d.toUTCString();
+
+  var obj = {'comment': comment, 'time': d.toUTCString()};
+
+  if (comments[context] === undefined)
+	comments[context] = [];	
+  comments[context].push(obj);
+  // JSON: Javascript Object Notation
+  res.send(JSON.stringify(comments[context]));
+});
+
+app.get('/fetchcomments', function(req, res) {
+  var context = req.query.context;
+	if (comments[context] !== undefined)
+	  res.send(JSON.stringify(comments[context]));
+	else {
+	  res.send("null");
+	}
 });
 
 app.get('/:articleName', function (req, res) {
-    // articleName == article-one
-    // articles[articleName] == {} content object for article one
-    var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+  var articleName = req.params.articleName;
+  if (articleName != 'favicon.ico')	{
+	  res.send(createTemplate(articles[articleName]));
+  }	
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -108,21 +118,10 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});  
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var comments= [];
-app.get('/submit', function(req , res) { 
-var cmnt= req.query.comment;
-comments.push(cmnt);
-res.send(JSON.stringify(comments));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
- 
