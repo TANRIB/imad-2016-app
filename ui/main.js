96c1a9f09c125d13a57b18bae3c47e509ca103c1
TANRIB <kibafathima@gmail.com>
2016-11-18 21:43:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,59 +1,100 @@
-// Counter code
-var button = document.getElementById('counter');
+// Eg: tanrib.imad.hasura-app.io/articles/article-one will result in article-one
+var currentArticleTitle = window.location.pathname.split('/')[2];
 
-button.onclick = function () {
+function loadCommentForm () {
+    var commentFormHtml = `
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <br/>
+        <input type="submit" id="submit" value="Submit" />
+        <br/>
+        `;
+    document.getElementById('comment_form').innerHTML = commentFormHtml;
     
-    // Create a request object
-    var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in a variable
-    request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          // Take some action
-          if (request.status === 200) {
-              var counter = request.responseText;
-              var span = document.getElementById('count'); 
-              span.innerHTML = counter.toString();
+    // Submit username/password to login
+    var submit = document.getElementById('submit');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+                // Take some action
+                if (request.status === 200) {
+                    // clear the form & reload all the comments
+                    document.getElementById('comment_text').value = '';
+                    loadComments();    
+                } else {
+                    alert('Error! Could not submit comment');
+                }
+                submit.value = 'Submit';
           }
-      }
-      // Not done yet
-   };
-    
-   // Make the request
-   request.open('GET', 'http://tanrib.imad.hasura-app.io/counter', true);
-   request.send(null);
-};    
+        };
+        
+        // Make the request
+        var comment = document.getElementById('comment_text').value;
+        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({comment: comment}));  
+        submit.value = 'Submitting...';
+        
+    };
+}
 
-// Submit name
-var submit = document.getElementById('submit_btn');
-submit.onclick = function () { 
-    
-    // Create a request object
+function loadLogin () {
+    // Check if the user is already logged in
     var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in a variable
     request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-           // Take some action
-           if (request.status === 200) {
-              // Capture a list of names and render it as a list
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list = '';
-              for(var i=0; i< names.length; i++) {
-                 list += '<li>' + names[i] + '</li>';
-              } 
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = list;
-          }
-      }
-      // Not done yet
-   };
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadCommentForm(this.responseText);
+            }
+        }
+    };
     
-   // Make the request
-   var nameInput = document.getElementById('name');
-   var name = nameInput.value; 
-   request.open('GET', 'http://tanrib.imad.hasura-app.io/submit-name?name='+ name, true);
-   request.send(null);
-   
-};
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+
+function escapeHTML (text)
+{
+    var $text = document.createTextNode(text);
+    var $div = document.createElement('div');
+    $div.appendChild($text);
+    return $div.innerHTML;
+}
+
+function loadComments () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var comments = document.getElementById('comments');
+            if (request.status === 200) {
+                var content = '';
+                var commentsData = JSON.parse(this.responseText);
+                for (var i=0; i< commentsData.length; i++) {
+                    var time = new Date(commentsData[i].timestamp);
+                    content += `<div class="comment">
+                        <p>${escapeHTML(commentsData[i].comment)}</p>
+                        <div class="commenter">
+                            ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
+                        </div>
+                    </div>`;
+                }
+                comments.innerHTML = content;
+            } else {
+                comments.innerHTML('Oops! Could not load comments!');
+            }
+        }
+    };
+    
+    request.open('GET', '/get-comments/' + currentArticleTitle, true);
+    request.send(null);
+}
+
+
+// The first thing to do is to check if the user is logged in!
+loadLogin();
+loadComments();
