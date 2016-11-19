9e45769c5d9d00d82a62c814bdca680b274ec19b
TANRIB <kibafathima@gmail.com>
2016-11-19 12:18:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'tanrib',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'db-tanrib-83634'
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
