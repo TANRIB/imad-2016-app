99a1d5b8013b1ca557f37daf8018aeeb2e2bf04d
TANRIB <kibafathima@gmail.com>
2016-11-20 11:40:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ submit.onclick = function () {
               alert('Logged in successfully');
               } else if (request.status === 403) {
                   alert('Username/passsword is incorrect');
-              } else if(request.sttus === 500) { 
+              } else if(request.status === 500) { 
                   alert('Something went wrong on the server');
               }
       }
@@ -24,6 +24,8 @@ submit.onclick = function () {
    // Make the request
    var username = document.getElementById('username').value;
    var password = document.getElementById('password').value;
+   console.log('username');
+   console.log('password');
    request.open('POST', 'http://tanrib.imad.hasura-app.io/login', true);
    request.setRequestHeader('Content-Type', 'application/json');
    request.send(JSON.stringify({username: username, password: password}));
