938f332926c0fea207f988bfb748d738d29eb1d6
TANRIB <kibafathima@gmail.com>
2016-11-26 22:01:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -148,9 +148,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+
 
 app.get('/blog.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
